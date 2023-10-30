6c1b10e23d11609efc14695ec56691abf4816d45
Mauri <mauri.monzon91@gmail.com>
2023-10-29 22:19:55 -0300

    Act

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 require("dotenv").config();
 const app = require("./src/app.js");
-// const postBulkGenres = require("./src/controllers/genre/postBulkGenresController.js");
-// const postBulkMovies = require("./src/controllers/movie/postBulkMoviesController.js");
-// const postBulkCandies = require("./src/controllers/candy/postBulkCandyController.js");
+const postBulkGenres = require("./src/controllers/genre/postBulkGenresController.js");
+const postBulkMovies = require("./src/controllers/movie/postBulkMoviesController.js");
+const postBulkCandies = require("./src/controllers/candy/postBulkCandyController.js");
 const { sequelize } = require("./src/db.js");
 
-async function main(){
-	try {
-		await sequelize.sync({ force: true });
-		console.log("Database synchronized successfully.");
-	
-		const PORT = process.env.PORT || 3001;
-		app.listen(PORT, () => {
-			console.log(`Server running on port ${PORT}`);
-		});
+async function main() {
+  try {
+    await sequelize.sync({ force: true });
+    console.log("Database synchronized successfully.");
 
-		// SE EJECTAN LAS FUNCIONES PARA QUE CREE AUTOMATICAMENTE LAS TABLAS AL INICIAR EL SERVER
-		// postBulkGenres();
-		// postBulkMovies();
-		// postBulkCandies();
-	} catch (error) {
-		console.error("Error synchronizing database:", error);
-	}
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+
+    // SE EJECTAN LAS FUNCIONES PARA QUE CREE AUTOMATICAMENTE LAS TABLAS AL INICIAR EL SERVER
+    postBulkGenres();
+    postBulkMovies();
+    postBulkCandies();
+  } catch (error) {
+    console.error("Error synchronizing database:", error);
+  }
 }
 
 main();
