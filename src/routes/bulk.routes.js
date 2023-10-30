591add6347ee31e671e839a533017d9d3e839f24
Mauri <mauri.monzon91@gmail.com>
2023-10-29 23:49:23 -0300

    a

--- src/routes/bulk.routes.js
+++
@@ -1,8 +0,0 @@
-const routerBulk = require("express").Router();
-
-const handlerPostBulk = require("../handlers/bulk/handlerPostBulk");
-
-
-routerBulk.get("/", handlerPostBulk);
-
-module.exports = routerBulk;
